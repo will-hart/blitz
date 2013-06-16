e9fac7bcf5d52a14bf42768e4bb1815fcc60f8d9
William Hart <hart.wl@gmail.com>
2013-06-16 22:42:14 +0100

    Draw chart background on SVG on load

--- static/js/blitz_gui_app.js
+++ static/js/blitz_gui_app.js
@@ -76,3 +76,48 @@ App.DataLine.FIXTURES = [{
     variableName: "Accelerator",
     variableValue: "0.05"
 }];
+
+/*********************************************************
+ * CHARTING FUNCTION
+*********************************************************/
+
+/* A function to generate chart axis */
+var chart_builder = function () {
+
+    /* set up the main chart variables and layout */
+    var margin = { top: 50, right: 50, bottom: 50, left: 50 },
+        width = innerWidth - margin.left - margin.right,
+        height = innerHeight - margin.top - margin.bottom,
+        parseDate = d3.time.format("%Y%m%d %h:%m%s").parse,
+        x = d3.scale.linear().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        colour = d3.scale.category10(),
+        xAxis = d3.svg.axis().scale(x).orient('bottom'),
+        yAxis = d3.svg.axis().scale(y).orient('left');
+
+    /* set up the line for drawing the series */
+    var line = d3.svg.line()
+        .interpolate("basis")
+        .x(function (d) { return x(d.timeLogged); })
+        .y(function (d) { return y(d.variableValue); });
+
+    /* add the axis to the SVG */
+    var svg = d3.select("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("viewBox", "0, 0, " + innerWidth + ", " + innerHeight)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    /* draw the x-axis */
+    svg.append("g")
+        .attr("class", "x_axis")
+        .attr("transform", "translate(0, " + height + ")")
+        .call(xAxis);
+
+    /* draw the y-axis */
+    svg.append("g")
+        .attr("class", "y_axis")
+        .call(yAxis);
+};
+
