943d27251a58dd5b4ccac00a262c3fb0560275e4
William Hart <hart.wl@gmail.com>
2013-06-17 23:08:10 +0100

    Draw chart in an ApplicationView

--- static/js/blitz_gui_app.js
+++ static/js/blitz_gui_app.js
@@ -20,6 +20,12 @@ App.DataLine = DS.Model.extend({
     variableValue: DS.attr('string')
 });
 
+/* The variable name model for tracking which variables are visible in the chart */
+App.LoggedVariables = DS.Model.extend({
+    variableName: DS.attr('string'),
+    visible: DS.attr('bool')
+})
+
 /* The settings model stores setting information */
 App.Config = DS.Model.extend({
     loggerPort: DS.attr('int'),
@@ -33,14 +39,15 @@ App.Config = DS.Model.extend({
  * CONTROLLERS
 *********************************************************/
 
-App.liveController = Ember.ArrayController.extend({
+App.ApplicationController = Ember.ArrayController.extend({
+
     content: [],
     displayed: [],
 
     /* Toggles whether a variable is displayed or not */
     updateDisplayed: function updateDisplayed(variableName) {
-        var disp = this.get('displayed');
-        var idx = disp.indexOf(variableName);
+        var disp = this.get('displayed'),
+            idx = disp.indexOf(variableName);
 
         // check if the variable name is in the box
         if (idx >= 0) {
@@ -49,31 +56,84 @@ App.liveController = Ember.ArrayController.extend({
             disp.push(variableName);
         }
 
-        console.log(disp);
         this.set('displayed', disp);
     }
 });
+
+App.ApplicationView = Ember.View.extend({
+
+    chart: {},
+    line: {},
+
+    /* Initialise and draw the chart  */
+    didInsertElement: function didInsertElement() {
+
+        /* set up the main chart variables and layout */
+        var margin = { top: 60, right: 60, bottom: 60, left: 60 },
+            width = innerWidth - margin.left - margin.right,
+            height = innerHeight - margin.top - margin.bottom,
+            x = d3.scale.linear().range([0, width]),
+            y = d3.scale.linear().range([height, 0]),
+            xAxis = d3.svg.axis().scale(x).orient('bottom'),
+            yAxis = d3.svg.axis().scale(y).orient('left'),
+
+            /* set up the line for drawing the series */
+            line = d3.svg.line()
+                .interpolate("basis")
+                .x(function (d) { return x(d.timeLogged); })
+                .y(function (d) { return y(d.variableValue); }),
+
+            /* add the axis to the SVG */
+            svg = d3.select("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .attr("viewBox", "0, 0, " + innerWidth + ", " + innerHeight)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        /* draw the x-axis */
+        svg.append("g")
+            .attr("class", "x_axis")
+            .attr("transform", "translate(0, " + height + ")")
+            .call(xAxis);
+
+        /* draw the y-axis */
+        svg.append("g")
+            .attr("class", "y_axis")
+            .call(yAxis);
+
+        /* dave the required variables */
+        this.set('chart', svg);
+        this.set('line', line);
+    },
+
+    /* Update the chart */
+    updateChart: function updateChart() {
+        var content = this.get('content'),
+            chart = this.get('chart'),
+            line = this.get('line');
+
+        chart.selectAll('path.line')
+            .data(content)
+            .transition()
+            .duration(500)
+            .ease('sin')
+            .attr('d', line(content));
+    }.observes('controllers.content.@each')
+});
+
 
 /*********************************************************
  * ROUTES
 *********************************************************/
 
-/* Set up the routes */
-App.Router.map(function () {
-    this.resource('live');
-});
-
-// live route shows main data page
-App.LiveRoute = Ember.Route.extend({
-    model: function () {
-        return App.DataLine.find();
-    }
-});
-
 // when opening the page go directly to the live route
 App.IndexRoute = Ember.Route.extend({
-    redirect: function () {
-        this.transitionTo('live');
+    model: function () {
+        return App.DataLine.find();
+    },
+    setupController: function (controller, index) {
+        controller.set('models', index.get('content'));
     }
 });
 
@@ -109,48 +169,4 @@ App.DataLine.FIXTURES = [{
     variableName: "Accelerator",
     variableValue: "0.05"
 }];
-
-/*********************************************************
- * CHARTING FUNCTION
-*********************************************************/
-
-/* A function to generate chart axis */
-var chart_builder = function () {
-
-    /* set up the main chart variables and layout */
-    var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-        width = innerWidth - margin.left - margin.right,
-        height = innerHeight - margin.top - margin.bottom,
-        parseDate = d3.time.format("%Y%m%d %h:%m%s").parse,
-        x = d3.scale.linear().range([0, width]),
-        y = d3.scale.linear().range([height, 0]),
-        colour = d3.scale.category10(),
-        xAxis = d3.svg.axis().scale(x).orient('bottom'),
-        yAxis = d3.svg.axis().scale(y).orient('left');
-
-    /* set up the line for drawing the series */
-    var line = d3.svg.line()
-        .interpolate("basis")
-        .x(function (d) { return x(d.timeLogged); })
-        .y(function (d) { return y(d.variableValue); });
-
-    /* add the axis to the SVG */
-    var svg = d3.select("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .attr("viewBox", "0, 0, " + innerWidth + ", " + innerHeight)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    /* draw the x-axis */
-    svg.append("g")
-        .attr("class", "x_axis")
-        .attr("transform", "translate(0, " + height + ")")
-        .call(xAxis);
-
-    /* draw the y-axis */
-    svg.append("g")
-        .attr("class", "y_axis")
-        .call(yAxis);
-};
 
