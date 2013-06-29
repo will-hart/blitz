c7f6bfbbc04d1c725fd486f124e135df4ae5097f
William Hart <hart.wl@gmail.com>
2013-06-29 16:21:01 +0100

    Add allowable commands to constants for error checking

--- blitz/constants.py
+++ blitz/constants.py
@@ -2,3 +2,27 @@ __author__ = 'Will Hart'
 
 COMMAND_MESSAGE_BYTES = 8
 SHORT_COMMAND_MESSAGE_BYTES = 2
+
+# commands that are valid to send TO the server
+# note only the command up to the space (if there is one) is included
+VALID_SERVER_COMMANDS = [
+    "START",
+    "STOP",
+    "STATUS",
+    "DOWNLOAD",
+    "BOARD",
+    "LOGGING"
+]
+
+# commands that are valid to send TO the client
+# note only the command up to the space (if there is one) is included
+VALID_CLIENT_COMMANDS = [
+    "ACK",
+    "NACK",
+    "INSESSION",
+    "NOSESSION",
+    "BOARDERROR",
+    "NOBOARD",
+    "ERROR",
+    "READY"
+]
