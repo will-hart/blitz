490cb93016a914f4ef25fde94b8ca7926a7094d9
William Hart <hart.wl@gmail.com>
2013-06-29 01:13:43 +0100

    Handle basic requests and states in TCP class.  Pythonic function names

--- blitz/io/client_states.py
+++ blitz/io/client_states.py
@@ -8,28 +8,28 @@ class BaseState(object):
     All methods return a BaseState derived object which should handle future message processing
     """
 
-    def EnterState(self, tcp):
+    def enter_state(self, tcp):
         """Called when entering the state"""
         return self
 
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         """Called when a message needs processing"""
         return self
 
-    def SendMessage(self, tcp, msg):
+    def send_message(self, tcp, msg):
         """
         Send the passed message over TCP and return the current state
         """
         tcp.send(msg)
         return self
 
-    def GoToState(self, tcp, state):
+    def go_to_state(self, tcp, state):
         """
-        Transition to a new state and call EnterState on it
+        Transition to a new state and call enter_state on it
 
         :return: the new state
         """
-        return state().EnterState(tcp)
+        return state().enter_state(tcp)
 
 
 class InitState(BaseState):
@@ -38,17 +38,17 @@ class InitState(BaseState):
     to the logger and waits for the response
     """
 
-    def EnterState(self, tcp):
+    def enter_state(self, tcp):
         """Send a logging query to the logger"""
-        return self.SendMessage(tcp, "LOGGING")
+        return self.send_message(tcp, "LOGGING")
 
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         if msg == "ACK":
             # logger is logging, transition to LOGGING state
-            return self.GoToState(tcp, LoggingState)
+            return self.go_to_state(tcp, LoggingState)
         elif msg == "NACK":
             # logger is not logging, go to idle
-            return self.GoToState(tcp, IdleState)
+            return self.go_to_state(tcp, IdleState)
         else:
             # no other messages are acceptable in this state
             raise Exception("Unable to process the given message from InitState: " + msg)
@@ -58,39 +58,40 @@ class IdleState(BaseState):
     """
     Handles the client idling, waiting for further commands
     """
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         # no server messages are acceptable in this state
         raise Exception("Received unexpected message in IdleState: " + msg)
 
-    def SendMessage(self, tcp, msg):
+    def send_message(self, tcp, msg):
         if msg == "START":
             tcp.send(msg)
-            return self.GoToState(tcp, StartingState)
+            return self.go_to_state(tcp, StartingState)
         elif msg[0:8] == "DOWNLOAD":
             tcp.send(msg)
-            return self.GoToState(tcp, DownloadingState)
+            return self.go_to_state(tcp, DownloadingState)
         else:
             raise Exception("Unknown message for IDLE state - " + msg)
 
 
 class StartingState(BaseState):
     """Handles logging starting - waits for ACK from server"""
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         if msg == "ACK":
-            return self.GoToState(tcp, LoggingState)
+            return self.go_to_state(tcp, LoggingState)
 
 
 class LoggingState(BaseState):
     """
     Handles the client in logging state - sends periodic status updates
     """
-    def SendMessage(self, tcp, msg):
+    def send_message(self, tcp, msg):
         if msg == "STOP":
             tcp.send("STOP")
-            return self.GoToState(tcp, StoppingState)
+            return self.go_to_state(tcp, StoppingState)
         elif msg == "STATUS":
             tcp.send("STATUS")
-            return self.GoToState(tcp, DownloadingState)
+        else:
+            tcp.parse_reading(msg)
         return self
 
 
@@ -98,9 +99,9 @@ class StoppingState(BaseState):
     """
     Handles waiting for acknowledgement from a client before entering IDLE state
     """
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         if msg == "ACK":
-            return self.GoToState(tcp, IdleState)
+            return self.go_to_state(tcp, IdleState)
         return self
 
 
@@ -108,13 +109,13 @@ class DownloadingState(BaseState):
     """
     Handles the client in logging state - sends periodic status updates
     """
-    def ProcessMessage(self, tcp, msg):
+    def process_message(self, tcp, msg):
         if msg == "NACK":
             # the data has been received
-            self.SendMessage(tcp, "ACK")
-            return self.GoToState(tcp, IdleState)
+            self.send_message(tcp, "ACK")
+            return self.go_to_state(tcp, IdleState)
 
         # otherwise we save the data row for processing
-        tcp.ParseReading(msg)
+        tcp.parse_reading(msg)
         return self
 
--- blitz/io/tcp.py
+++ blitz/io/tcp.py
@@ -1,9 +1,142 @@
 __author__ = 'Will Hart'
 
+import socket
+import threading
+import SocketServer
 
-class TcpServer(object):
-    pass
+from blitz.io.client_states import *
+
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+    """
+    A class for handling TCP requests
+    """
+
+    def handle(self):
+        """
+        Handle the request - in this case just echo the result
+        """
+        while True:
+            data = self.request.recv(1024)
+
+            # check if the connection is still alive
+            if not data:
+                break
+            cur_thread = threading.current_thread()
+
+            response = "{}: {}".format(cur_thread.name, data)
+
+            # echo the response back to the server
+            self.request.sendall(response)
+
+
+class TcpServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    def __init__(self, address):
+        """
+        Creates a new TCP server
+        """
+        SocketServer.TCPServer.__init__(self, address, ThreadedTCPRequestHandler)
 
 
 class TcpClient(object):
-    pass
+    """
+    A TCP client which maintains a socket connection and
+    sends / receives messages upon request
+    """
+
+    current_state = None
+
+    def __init__(self, address):
+        """
+        Connect the socket to the given port and IP
+        """
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect(address)
+        self.connected = True
+
+        # start up the state machine
+        self.current_state = InitState().enter_state(self)
+
+    def send(self, message):
+        """
+        Send the given message and read the echoed response
+        """
+
+        if not self.connected:
+            raise Exception("Attempted to send data on a closed socket!")
+
+        try:
+            print "Sending: {}".format(message)
+            self._socket.sendall(message)
+            response = self._socket.recv(1024)
+            print "Received: {}".format(response)
+        except Exception as e:
+            print "An error occurred - {}".format(e)
+            print " >> Closing the socket"
+            self._socket.close()
+
+    def disconnect(self):
+        """
+        Disconnects the socket
+        """
+        self._socket.shutdown(socket.SHUT_RDWR)
+        self._socket.close()
+        self.connected = False
+
+    def parse_reading(self):
+        """
+        Parses a reading received from a state machine using ExpansionBoard
+        classes.
+        """
+        # TODO
+        pass
+
+    def request_status(self):
+        """
+        Request a status update from the data logger
+        """
+        self.current_state = self.current_state.send_message(self, "STATUS")
+
+    def request_download(self, session_id):
+        """
+        Request download of a session of data from the data logger
+        """
+        self.current_state = self.current_state.send_message(self, "DOWNLOAD " + str(session_id))
+
+    def request_start(self):
+        """
+        Requests logging to start
+        """
+        self.current_state = self.current_state.send_message(self, "START")
+
+    def request_stop(self):
+        """
+        Requests logging to stop
+        """
+        self.current_state = self.current_state.send_message(self, "STOP")
+
+    def request_is_logging(self):
+        """
+        Asks the server if it is currently logging
+        """
+        self.current_state = self.current_state.go_to_state(self, InitState)
+
+
+# EXAMPLE USAGE:
+
+# from blitz.io.tcp import TcpServer, TcpClient
+# ip = "localhost"
+# port = 8989
+# addr = (ip, port)
+#
+# import threading
+# server = TcpServer(addr)
+# server_thread = threading.Thread(target=server.serve_forever)
+# server_thread.daemon = True
+# server_thread.start()
+#
+# client = TcpClient(addr)
+# client.send("Hello World 1")
+# client.send("Hello World 2")
+# client.send("Hello World 3")
+
