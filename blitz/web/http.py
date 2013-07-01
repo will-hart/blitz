7cd576c3a7566d6d0331209dc6bbbbfaf41995ec
William Hart <hart.wl@gmail.com>
2013-07-01 18:44:09 +0100

    Add an HTTP handler for a connect request (toggles connection)

--- blitz/web/http.py
+++ blitz/web/http.py
@@ -1,7 +1,10 @@
 __author__ = 'Will Hart'
 
+import json
 import os
 from tornado.web import RequestHandler
+
+from blitz.io.tcp import TcpClient
 
 
 class IndexHandler(RequestHandler):
@@ -10,3 +13,23 @@ class IndexHandler(RequestHandler):
         # just read the file in to prevent tornado from processing handlebars
         resp_file = open(os.path.join(self.application.settings['template_path'], "index.html"))
         self.write(resp_file.read())
+
+
+class ConnectHandler(RequestHandler):
+
+    def get(self):
+        """Toggles the TCP connection"""
+        tcp = self.application.settings['socket']
+
+        if tcp is None:
+            # we are connecting
+            tcp = TcpClient("127.0.0.1", 8999)  # TODO get from config
+            self.application.settings['socket'] = tcp
+            response = { 'connected': False }
+        else:
+            tcp.disconnect()
+            self.application.settings['socket'] = None
+            response = { 'connected': True }
+
+        self.content_type = "application/json"
+        self.write(json.dumps(response))
