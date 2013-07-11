2e8aa4c8769f58b361b88cf59b753a15efbee879
William Hart <hart.wl@gmail.com>
2013-07-11 23:00:17 +0100

    Pause on generating status update

--- blitz/web/api.py
+++ blitz/web/api.py
@@ -8,14 +8,20 @@ from blitz.data.models import *
 
 
 class ApiRequestHandler(RequestHandler):
-    def generate_status_response(app):
+    def generate_status_response(self):
         """generates a status response"""
-        tcp = app.application.settings['socket']
-        data = app.settings['data']
+        tcp = self.application.settings['socket']
+        data = self.settings['data']
 
         if tcp is None:
             time.sleep(1.0)  # let tcp get populated?
-            tcp = app.application.settings['socket']
+            tcp = self.application.settings['socket']
+
+        # if the tcp is doing something, wait for it to finish before checking status
+        # but don't wait too long or the browser will get bored :)
+        wait_counter = 0
+        while tcp is not None and tcp.waiting and wait_counter < 20:
+            time.sleep(0.1)
 
         response = {
             "logging": False if tcp is None else tcp.is_logging(),
