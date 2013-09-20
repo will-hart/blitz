cc0ecac551a6febb5b0780d321fa7043ff33b8f8
will-hart <hart.wl@gmail.com>
2013-09-20 13:11:43 +0100

    Dots at line points

--- blitz/ui/windows.py
+++ blitz/ui/windows.py
@@ -46,8 +46,8 @@ class BlitzLoggingWidget(Qt.QWidget):
 
         # add the lines
         self.lines = []
-        self.lines += self.axis.plot([0, 1, 1.5, 6, 3, 3.2, 3.8, 4.8, 5.3, 5.2, 5.1, 3, 2, 0])
-        self.lines += self.axis.plot([1, 1.5, 6, 3, 3.2, 3.8, 4.8, 5.3, 5.2, 5.1, 3, 2, 0, 0])
+        self.lines += self.axis.plot([0, 1, 1.5, 6, 3, 3.2, 3.8, 4.8, 5.3, 5.2, 5.1, 3, 2, 0], 'o-')
+        self.lines += self.axis.plot([1, 1.5, 6, 3, 3.2, 3.8, 4.8, 5.3, 5.2, 5.1, 3, 2, 0, 0], 'o-')
 
         # create the canvas
         self.canvas = FigureCanvas(self.figure)
