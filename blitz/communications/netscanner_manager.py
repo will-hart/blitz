8017a20b5ee66d8fd251d55642d0391f3bfa28a7
will-hart <hart.wl@gmail.com>
2014-03-07 21:42:29 +0000

    Full functional NetScanner manager

--- blitz/communications/netscanner_manager.py
+++ blitz/communications/netscanner_manager.py
@@ -8,42 +8,14 @@
 
 __author__ = 'Will Hart'
 
+import datetime
 import logging
 import Queue
-import threading
 import socket
+import threading
 import time
 
-
-class NetScannerResultWrapper(object):
-    """
-    A wrapper around a list of NetScanner results
-    """
-
-    # psi to kPa conversion
-    psi_to_kpa = 6.89475729
-
-    def __init__(self, results=None):
-        self.channels = [x * self.psi_to_kpa for x in results]
-
-    def channel(self, channel_id):
-        """
-        Returns a single channel from the results, or -1 if the channel number is invalid
-
-        :param channel_id: The channel number to get the pressure value for
-        :returns: A single float value, indicating the channel pressure reading in kPa
-        """
-        if channel_id < 0 or channel_id > len(self.channels):
-            return -1
-        return self.channels[channel_id]
-
-    def results(self):
-        """
-        Returns all of the results from this NetScanner dataset
-
-        :returns: A list of float values indicating kPa pressures
-        """
-        return self.channels
+from blitz.communications.signals import logging_started
 
 
 class NetScannerManager(object):
@@ -58,7 +30,13 @@ class NetScannerManager(object):
     #  3. Set standard units (e.g kPa)
     #  4. Calibrate offset (i.e. set ambient to 0)
     #  5. Query data from all channels in decimal format (repeat)
-    INIT_SEQUENCE = ['A', 'B', 'v01101 6.894757', 'h', 'rFFFF0']
+    INIT_SEQUENCE = [
+        ('A', 'NOP connection check'),
+        ('B', 'reset device'),
+        ('v01101 6.894757', 'use kPa'),
+        ('h', 'zero offsets'),
+        ('b', 'digital read data')
+    ]
 
     REQUEST_TIMEOUT = 3.0
 
@@ -66,21 +44,25 @@ class NetScannerManager(object):
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self, host, port=9000):
+    def __init__(self, host, port=9000, database=None):
         """
         Initialises a NetScannerManager which connects a TCP/IP connection to the device
 
         :param host: The host IP address of the NetScanner device
         :param port: The port of the NetScanner device
+        :param database: The database to use to save serial data
         """
 
         self.__host = host
         self.__port = port
+        self.__data = database
         self.receive_queue = Queue.Queue()
         self.__stop_event = threading.Event()
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__socket.settimeout(self.REQUEST_TIMEOUT)
         self.__run_thread(self.run_client)
+
+        logging_started.connect(self.start_logging)
 
     def __run_thread(self, thread_target):
         self.__thread = threading.Thread(target=thread_target, args=[self.__stop_event])
@@ -88,13 +70,16 @@ class NetScannerManager(object):
         self.__thread.start()
 
     def run_client(self, stop_event):
-        print ("NetScanner interface starting")
         self.__socket.connect((self.__host, self.__port))
+        self.__logging_start = datetime.datetime.now()
+
         current_state = 0
-        print ("Netscanner connected")
+        self.logger.debug("NetScanner starting polling loop")
 
         while not stop_event.is_set():
-            self.__socket.send(self.INIT_SEQUENCE[current_state])
+            self.__socket.send(self.INIT_SEQUENCE[current_state][0])
+            if current_state < len(self.INIT_SEQUENCE) - 1:
+                self.logger.debug("Netscanner sent {0} message".format(self.INIT_SEQUENCE[current_state][1]))
             data = self.__socket.recv(1024)
             self.receive_message(data)
 
@@ -105,9 +90,14 @@ class NetScannerManager(object):
                 time.sleep(1.0 / self.SAMPLE_FREQUENCY)
 
         # terminate the context before exiting
-        print ("Terminating NetScanner thread")
         self.__socket.close()
-        print ("NetScanner terminated")
+        self.logger.debug("NetScanner terminated")
+
+    def start_logging(self, args):
+        """
+        Stores the current time when data logging commences so the correct timestamp can be provided to messages
+        """
+        self.__logging_start = datetime.datetime.now()
 
     def receive_message(self, message):
         """
@@ -115,12 +105,23 @@ class NetScannerManager(object):
 
         :param message: the message that was received
         """
-        print "Message length %s received" % len(message)
-        if message[0] in self.response_codes.keys():
-            print ("NetScanner received message: %s" % self.response_codes[message[0]])
+        if message == "A":
+            self.logger.debug("NetScanner received ACK from device")
         else:
-            results = [float(x) for x in message.split(" ")]
-            self.receive_queue.put(NetScannerResultWrapper(results))
+            if self.__data:
+                delta_t = (datetime.datetime.now() - self.__logging_start).microseconds / 1000.0
+                delta_t = hex(delta_t).rjust(8, '0').upper()
+                results = [message[i:i+4] for i in xrange(0, len(message), 4)]
+
+                if len(results) != 16:
+                    self.logger.warning(
+                        "Received incomplete NetScanner message, only read %s channels (not 16)" % len(results))
+                else:
+                    # hackity hack
+                    self.__data.queue("0A0ABO" + delta_t + results[0] + results[1] + results[2] + results[3])
+                    self.__data.queue("0A0AA8" + delta_t + results[4] + results[5] + results[6] + results[7])
+                    self.__data.queue("0A0AA4" + delta_t + results[8] + results[9] + results[10] + results[11])
+                    self.__data.queue("0A0AA2" + delta_t + results[12] + results[13] + results[14] + results[15])
 
     def stop_client(self):
         """
@@ -128,14 +129,4 @@ class NetScannerManager(object):
         """
         self.__stop_event.set()
         self.__thread.join()
-        print ("NetScanner thread stopped")
-
-    def get_channels(self):
-        """
-        Gets the FIFO based list of channel pressures
-        """
-
-        if not self.receive_queue.empty():
-            return self.receive_queue.get().results()
-        else:
-            return []
+        self.logger.debug("NetScanner thread stopped")
