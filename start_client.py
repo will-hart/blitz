d2cbb82a1b8567d0e2f0d8f0fd591179e7a7c6db
Will Hart <hart.wl@gmail.com>
2014-03-11 23:35:51 +0000

    Warn about the web UI (deprecated)

--- start_client.py
+++ start_client.py
@@ -13,5 +13,12 @@ def close_io_loop():
 signal.signal(signal.SIGTERM, close_io_loop)
 
 if __name__ == "__main__":
+    
+    print "**********************WARNING**********************"
+    print "The Web interface is deprecated and unspported. In"
+    print "the long term the web interface will be reinstated,"
+    print "however at this time only the desktop UI should be "
+    print "used"
+    print "**********************WARNING**********************"
     app = WebApplicationClient()
     app.run()
