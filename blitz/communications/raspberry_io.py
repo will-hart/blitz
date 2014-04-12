d043f61e453112b90a6ab2605129900de0f91cd2
will-hart <hart.wl@gmail.com>
2014-04-12 15:11:23 +0100

    Implement GPIO access

--- blitz/communications/raspberry_io.py
+++ blitz/communications/raspberry_io.py
@@ -2,11 +2,11 @@ __author__ = 'Will Hart'
 
 # we should only import this Raspberry Pi manager if RPi.GPIO is installed
 try:
-    import RPi.GPIO as gpio
+    import RPi.GPIO as GPIO
 except ImportError:
-
     pass
 else:
+
     import datetime
     import logging
     import threading
@@ -54,9 +54,50 @@ else:
 
         def run_client(self, stop_event):
 
-            self.logger.debug("Starting Raspberry Pi GPIO polling")
+            self.logger.debug("Configuration RaspberryPi IO Ports")
 
+            # set up the board numbering mode and put pins in the correct mode
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(4, GPIO.IN)
+            GPIO.setup(17, GPIO.IN)
+            GPIO.setup(18, GPIO.IN)
+            GPIO.setup(21, GPIO.IN)
+            GPIO.setup(22, GPIO.IN)
+            GPIO.setup(23, GPIO.IN)
+            GPIO.setup(24, GPIO.IN)
+            GPIO.setup(25, GPIO.IN)
+            GPIO.setup(30, GPIO.IN)
+            GPIO.setup(31, GPIO.IN)
+
+            self.logger.debug("Starting Raspberry Pi GPIO polling")
             while not stop_event.is_set():
+
+                # read in the pins
+                results = [
+                    GPIO.setup(4, GPIO.IN),
+                    GPIO.setup(17, GPIO.IN),
+                    GPIO.setup(18, GPIO.IN),
+                    GPIO.setup(21, GPIO.IN),
+                    GPIO.setup(22, GPIO.IN),
+                    GPIO.setup(23, GPIO.IN),
+                    GPIO.setup(24, GPIO.IN),
+                    GPIO.setup(25, GPIO.IN)
+                ]
+
+                # build up a single object
+                result = 0
+                for r in results:
+                    result = (result | r) << 1
+
+                # build the base message
+                delta_t = (datetime.datetime.now() - self.__logging_start).total_seconds() * 1000.0
+                formatted_result = "03A0" + hex(int(delta_t))[2:].rjust(8, '0').upper()
+                formatted_result += hex(int(result))[2:].rjust(2, '0').upper()
+
+                # store the result
+                self.__data.queue(formatted_result)
+
+                # sleepy time
                 time.sleep(1.0 / self.SAMPLE_FREQUENCY)
 
             # terminate the context before exiting
@@ -67,7 +108,7 @@ else:
             Stores the current time when data logging commences so the correct timestamp can be provided to messages
             """
             self.__logging_start = datetime.datetime.now()
-            self.__run_thread()
+            self.__run_thread(self.run_client)
 
 
         def receive_message(self, channels):
